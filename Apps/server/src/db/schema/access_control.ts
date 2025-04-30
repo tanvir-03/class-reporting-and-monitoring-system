45f3dd6e91b25ad42bd794b2ba4e883a4bfa839a
Ahsanul Mahbub Tanvir <tanvir1998314@gmail.com>
2025-04-30 07:17:53 +0600

    added schema for policy based access control system.

---
+++ Apps/server/src/db/schema/access_control.ts
@@ -0,0 +1,37 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { auditColumns } from "./audit_columns";
+import { user } from "./auth";
+
+export const role = sqliteTable("role", {
+	id: text("id").primaryKey(),
+	name: text("name").notNull().unique(),
+	...auditColumns,
+});
+
+export const permission = sqliteTable("permission", {
+	id: text("id").primaryKey(),
+	name: text("name").notNull().unique(),
+	...auditColumns,
+});
+
+export const rolePermission = sqliteTable("role_permission", {
+	id: text("id").primaryKey(),
+	roleId: text("role_id")
+		.notNull()
+		.references(() => role.id),
+	permissionId: text("permission_id")
+		.notNull()
+		.references(() => permission.id),
+	...auditColumns,
+});
+
+export const userRole = sqliteTable("user_role", {
+	id: text("id").primaryKey(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => user.id),
+	roleId: text("role_id")
+		.notNull()
+		.references(() => role.id),
+	...auditColumns,
+});
